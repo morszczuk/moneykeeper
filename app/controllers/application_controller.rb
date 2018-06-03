258e303e6cdffdc6f9d721b46d37dc67e666614e
Miron Marczuk <you@example.com>
2018-06-03 13:59:41 +0200

    basic auth added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_authentication
+    
+  def require_authentication
+      authenticate_or_request_with_http_basic do |u,p|
+          true if u==ENV['AUTH_USER'] && p==ENV['AUTH_PASSWORD']
+      end
+  end
+  # http_basic_authenticate_with user: ENV["AUTH_USER"].to_s, password: ENV["AUTH_PASSWORD"].to_s
 end
